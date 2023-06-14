aed1311b4f7adde3d6c62911f0c4043c7df74417
MaximTuktarev <maxim.tuktarev@mail.ru>
2023-06-14 18:23:41 +0400

    task js complete

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,99 @@
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
+
+	avg = +(sum / arr.length).toFixed(2);
+	return {
+		min: min,
+		max: max,
+		avg: avg,
+	};
 }
+
+// Задача 2
+  //  summElementsWorker - находит сумму элементов массива и возвращать её
 
 function summElementsWorker(...arr) {
-
+	let array = [...arr];
+	if (array.length === 0) {
+		return 0
+	}
+	let summ = array.reduce((sum, current) => sum + current, 0);
+	return summ
 }
+
+  //  differenceMaxMinWorker - вычисленият разницы максимального и минимального элементов
 
 function differenceMaxMinWorker(...arr) {
-
+	let array = [...arr];
+	if (array.length === 0) {
+		return 0
+	}
+	let newMin = Math.min(...array);
+	let newMax = Math.max(...array);
+	return (newMax - newMin)
 }
+
+  // differenceEvenOddWorker - вычислениt разницы сумм чётных и нечётных элементов
 
 function differenceEvenOddWorker(...arr) {
-
+	let array = [...arr];
+	if (array.length === 0) {
+		return 0
+	}
+	let sumOddElemet = 0;
+	let sumEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElemet += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElemet;
 }
+
+  //  averageEvenElementsWorker - среднее значение чётных элементов
 
 function averageEvenElementsWorker(...arr) {
-
+	let array = [...arr];
+	if (array.length === 0) {
+		return 0
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement = countEvenElement + 1;
+		}
+	}
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+// Задача 3
 
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	let array = [...arrOfArr];
+	for (let i = 0; i < array.length; i++) {
+		work = func(...array[i]);
+		if (work > maxWorkerResult) {
+			maxWorkerResult = work;
+		}
+	}
+	return maxWorkerResult;
 }
